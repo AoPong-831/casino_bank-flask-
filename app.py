7e96e349feee03861c5766d82bffe9f6464aa542
AoPong-831 <newipadmail114514@gmail.com>
2022-04-15 18:00:43 +0900

    DB作成中(errorあり)

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 from flask import Flask
 from flask import render_template, request, redirect
+#from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import db
+
+DATABASE = "database.db"
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db.create_bank()
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 
 @app.route("/ranking")
 def ranking():
     return render_template("ranking.html")
-    
+
+
+@app.route("/withdrawal")
+def withdrawal():
+    return render_template("withdrawal.html")
+
+
+@app.route("/deposit")
+def deposit():
+    return render_template("deposit.html")
+
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
