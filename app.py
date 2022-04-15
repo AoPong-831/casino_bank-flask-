ac401ab61ad00b2bf9bc1778f2d08d9b45eaf396
AoPong-831 <newipadmail114514@gmail.com>
2022-04-16 04:18:32 +0900

    新規登録まで

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import render_template, request, redirect
 import sqlite3
 import db
 
-DATABASE = "database.db"
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+DATABASE = "database.db"
 db.create_bank()
+
+error_text = ""#エラー時にerror.htmlで表示するテキスト
 
 @app.route("/")
 def index():
@@ -35,8 +37,31 @@ def deposit():
     return render_template("deposit.html")
 
 
-@app.route("/create")
+@app.route("/create",methods=["GET","POST"])
 def create():
+    if request.method == "POST":
+        name = request.form["name"]#送信されたnameを受け取る
+        if name == "":#nameが空欄だったら
+            print("==error==")
+        else:
+            con = sqlite3.connect(DATABASE)#データベースと接続
+            list_name = con.execute("select name from user_table")
+            name_match_flg = False#名前がダブった判定
+            for n in list_name:#なぜかnはlistになってる
+                if n[0] == name:
+                    name_match_flg = True
+                    error_text = "この名前は既に使われています"
+                    return render_template("error.html",error_text=error_text)
+            
+            if not(name_match_flg):#名前がダブらなかったとき
+                con.execute("insert into user_table values(?,?)",[name,1000])
+                con.commit()
+                a = con.execute("select * from user_table")
+                for b in a:
+                    print(b)
+        
+            print(name_match_flg)
+            con.close()
     return render_template("create.html")
 
 
---
+++ db.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+DATABASE = "database.db"
+
+def create_bank():
+    con = sqlite3.connect(DATABASE)
+    con.execute("CREATE TABLE IF NOT EXISTS user_table (name, money)")
+    con.close()
